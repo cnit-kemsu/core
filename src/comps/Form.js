b95cc58a1dab0f3600045db86f31d23d8acc0c4b
John Cooper <johnxcooper87@gmail.com>
2019-05-16 13:55:05 +0700

    update

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import FormActions from './FormActions';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 
 function Form({ form, title, children, submitText, submitIcon, actions }) {
+  const submitOnEnter = useCallback(
+    event => {
+      if (event.key === 'Enter') form.submit();
+    },
+    []
+  );
   
   return (
-    <>
+    <span onKeyPress={submitOnEnter}>
       {title && <DialogTitle>{title}</DialogTitle>}
 
       <DialogContent>
@@ -17,7 +23,7 @@ function Form({ form, title, children, submitText, submitIcon, actions }) {
       <DialogActions>
         <FormActions {...{ form, submitText, submitIcon, actions }} />
       </DialogActions>
-    </>
+    </span>
   );
 }
 
