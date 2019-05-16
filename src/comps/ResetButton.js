d44fe2275b0bcbbbfcff2702133ca7da88fa8ec6
John Cooper <johnxcooper87@gmail.com>
2019-05-16 11:10:39 +0700

    update

--- src/comps/ResetButton.js
+++ src/comps/ResetButton.js
@@ -3,11 +3,11 @@ import Button from '@material-ui/core/Button';
 import ClearIcon from '@material-ui/icons/Clear';
 import { useFormStatus } from '@kemsu/form';
 
-function ResetButton({ form, children = 'Сбросить', ...props }) {
+function ResetButton({ form, children = 'Сбросить', disabled, ...props }) {
   const { dirty } = useFormStatus(form);
 
   return <Button size="small" color="secondary"
-    data-control disabled={!dirty} onClick={form.reset} {...props}
+    data-control disabled={!dirty || disabled} onClick={form.reset} {...props}
   >
     <ClearIcon />
     {children}
--- src/comps/SubmitFab.js
+++ src/comps/SubmitFab.js
@@ -3,10 +3,10 @@ import SendIcon from '@material-ui/icons/Send';
 import { useFormStatus } from '@kemsu/form';
 import Fab from './Fab';
 
-function SubmitFab({ form, icon = SendIcon, children = 'Отправить', ...props }) {
-  const { hasErrors, touched } = useFormStatus(form);
+function SubmitFab({ form, icon = SendIcon, children = 'Отправить', disableIfNotDirty = false, disabled, ...props }) {
+  const { hasErrors, touched, dirty } = useFormStatus(form);
 
-  return <Fab icon={icon} data-control disabled={hasErrors && touched} onClick={form.submit} {...props}>
+  return <Fab icon={icon} data-control disabled={(hasErrors && touched) || (disableIfNotDirty && !dirty) || disabled} onClick={form.submit} {...props}>
     {children}
   </Fab>;
 }
