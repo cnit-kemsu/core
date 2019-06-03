cff9b96279e9e75d53296b36de2d91befc638b68
John Cooper <johnxcooper87@gmail.com>
2019-06-03 14:08:08 +0700

    update

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,23 +1,17 @@
-import React, { useCallback, useContext } from 'react';
+import React from 'react';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import { ComposerContext } from '@kemsu/form';
+import { useEnterClickSubmit } from '@kemsu/form';
 import FormActions from './FormActions';
 import FormErrors from './FormErrors';
 import { Form as useStyles } from './styles';
 
 function Form({ comp, title, children, submitText, submitIcon, resetText, disableSubmitIfNotDirty }) {
-  const _comp = comp || useContext(ComposerContext);
-  const submitOnEnter = useCallback(
-    event => {
-      if (event.key === 'Enter') _comp.submit();
-    },
-    []
-  );
+  const handleEnterKeyPress = useEnterClickSubmit(comp);
   
   const classes = useStyles({ resetText });
-  return <span onKeyPress={submitOnEnter}>
+  return <span onKeyPress={handleEnterKeyPress}>
     {title && <DialogTitle>{title}</DialogTitle>}
 
     <DialogContent>
@@ -25,8 +19,8 @@ function Form({ comp, title, children, submitText, submitIcon, resetText, disabl
     </DialogContent>
 
     <DialogActions className={classes.actions}>
-      <FormErrors comp={_comp} />
-      <FormActions {...{ comp: _comp, submitText, submitIcon, resetText, disableSubmitIfNotDirty }} />
+      <FormErrors comp={comp} />
+      <FormActions {...{ comp, submitText, submitIcon, resetText, disableSubmitIfNotDirty }} />
     </DialogActions>
   </span>;
 }
