88576df52cf4c023169c1bf5aaf97895ef4bf8f7
unknown <johnxcooper87@gmail.com>
2020-01-23 15:36:05 +0700

    update

--- src/hooks/useElementArray.js
+++ src/hooks/useElementArray.js
@@ -5,23 +5,36 @@ function defaultKey({ id }) {
   return id;
 }
 
-export function useElementArray(renderElement, array = [], { key = defaultKey, memoize = true, ...props }) {
+// export function useElementArray(renderElement, array = [], { key = defaultKey, memoize = true, ...props }) {
 
-  const _props = React.useRef();
-  _props.current = props;
-  const getProps = React.useCallback(() => _props.current, [])
+//   const _props = React.useRef();
+//   _props.current = props;
+//   const getProps = React.useCallback(() => _props.current, [])
 
-  const component = (({ item }) => renderElement(item, getProps()))
-  |> (() => memo(#, memoize))
-  |> useMemo(#, []);
+//   const component = (({ item }) => renderElement(item, getProps()))
+//   |> (() => memo(#, memoize))
+//   |> useMemo(#, []);
 
-  const createElement = (
-    (item, index) => React.createElement(component, {
-      key: key(item),
-      item: item.constructor === Object ? { index, ...item } : item
-    })
-  ) |> useCallback(#, []);
+//   const createElement = (
+//     (item, index) => React.createElement(component, {
+//       key: key(item),
+//       item: item.constructor === Object ? { index, ...item } : item
+//     })
+//   ) |> useCallback(#, []);
 
-  return (() => array && array.map(createElement))
-  |> useMemo(#, [array]);
+//   return (() => array && array.map(createElement))
+//   |> useMemo(#, [array]);
+// }
+
+export function useElementArray(renderElement, array = [], { key = defaultKey, ...props }) {
+
+  const _renderElement = useCallback(
+    ({ item, _props }) => renderElement(item, _props),
+    []
+  );
+  return array && array.map((item, index) => React.createElement(_renderElement, {
+    key: key(item, index),
+    item: item.constructor === Object ? { index, ...item } : item,
+    _props: props
+  }));
 }
